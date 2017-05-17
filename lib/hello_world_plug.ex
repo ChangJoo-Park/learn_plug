fe704100672ca06e050c96e61f187da86990f0e0
ChangJoo Park <pcjpcj2@gmail.com>
2017-05-17 20:01:55 +0900

    :heavy_plus_sign: Add hello route

---
+++ lib/hello_world_plug.ex
@@ -0,0 +1,11 @@
+defmodule LearnPlug.HelloWorldPlug do
+  import Plug.Conn
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plan")
+    |> send_resp(200, "Hello World!")
+  end
+end
---
+++ lib/router.ex
@@ -0,0 +1,18 @@
+defmodule LearnPlug.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Welcome")
+  end
+
+  get "/hello" do
+    send_resp(conn, 200, "Hello Elixir!")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+end
