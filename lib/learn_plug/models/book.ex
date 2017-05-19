994e9b7161ee7d150c06c9ddc6bb4c71e53aeb99
ChangJoo Park <pcjpcj2@gmail.com>
2017-05-20 08:39:29 +0900

    :heavy_plus_sign: Update JSON Book Routes

--- lib/learn_plug/models/book.ex
+++ lib/learn_plug/models/book.ex
@@ -1,6 +1,7 @@
 defmodule LearnPlug.Book do
   use Ecto.Schema
 
+  @derive { Poison.Encoder, except: [:__meta__] }
   schema "books" do
     field :name, :string
     field :author, :string
--- lib/routes/book_routes.ex
+++ lib/routes/book_routes.ex
@@ -1,21 +1,22 @@
 defmodule LearnPlug.BookRoutes do
   use Plug.Router
+  alias LearnPlug.Book
+  alias LearnPlug.Repo
 
   plug :match
   plug :dispatch
 
   get "/" do
+    books = Repo.all(Book)
     conn
-    |> send_resp(200, "Book")
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(books))
   end
 
   get "/:id" do
+    book = Repo.get(Book, id)
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode!(%{
-      :name => "The Interpretation of Dreams",
-      :author => "Sigmund Freud",
-      :publishedAt => "1900"
-    }))
+    |> send_resp(200, Poison.encode!(book))
   end
 end
