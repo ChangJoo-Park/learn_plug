65b5f18e05bc10b120128960fc3e1c332cfd5361
ChangJoo Park <pcjpcj2@gmail.com>
2017-05-19 22:57:22 +0900

    :heavy_plus_sign: Add migration

--- priv/repo/migrations/20170519134735_add_books_table.exs
+++ priv/repo/migrations/20170519134735_add_books_table.exs
@@ -2,6 +2,13 @@ defmodule LearnPlug.Repo.Migrations.AddBooksTable do
   use Ecto.Migration
 
   def change do
+    create table(:books) do
+      add :name, :string
+      add :author, :string
+      add :language, :string
+      add :isbn, :string
 
+      timestamps()
+    end
   end
 end
